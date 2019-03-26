ff63b94bbc724ac783bb062ebce755c3f8d80922
Chad Collins <ch.collins@outlook.com>
2019-03-26 16:45:32 -0400

    created grid for album view.

--- src/main/resources/static/js/components/Albums.js
+++ src/main/resources/static/js/components/Albums.js
@@ -2,7 +2,7 @@ import Songs from './Songs'
 
 export default function Albums(albums) {
     return `
-      <ul class="albums" class="grid-list">
+      <ul id="albums" class="grid-list">
         ${albums.map(album => {
         return `
                 <li id="album" class="album grid-list--item">
@@ -10,7 +10,7 @@ export default function Albums(albums) {
                     <img class="grid-image" src="${album.albumCover}" alt="Album Cover">
                     <h3  class="item-name">${album.albumTitle}</h3>
                   </div>
-                <ul class="songs">
+                <ul class="songs hidden">
                     ${Songs(album.songs)}
                 </ul>
                 </li>
