76f068cca755d6ca31c110f96ed8a9a2358991b1
Jason Hochheiser <jsunh98@gmail.com>
2019-03-28 15:32:17 -0400

    ready for presentation

--- src/main/resources/static/js/components/Artists.js
+++ src/main/resources/static/js/components/Artists.js
@@ -22,7 +22,7 @@ export default function Artists(artists) {
                         <ul>
                           <li>Artist Rating: ${artist.artistRating}/10</li>
                           <li>Albums: ${artist.albums.length}</li>
-                          <li>${Tags(artist.tags)}</li>  
+                          <li>${Tags(artist.tags)}</li>
                           <li class="artistCommentOn">Comments</li>
                           <li class="artistCommentOff hidden">Close Comments</li>
                         </ul>
@@ -41,8 +41,8 @@ export default function Artists(artists) {
                       <div class="modal-content--body comments hidden">${ArtistComments(artist.artistComments)}
                                <section class="add-artist-comment">
                                <input class= "add-artist-comment__comment" type = "text" placeholder= "Type your comment here.">
-                               <input class= "hidden add-artist-comment__artist" type = "text" placeholder= "Artist Name" value="${artist.artistId}">  
-                              
+                               <input class= "hidden add-artist-comment__artist" type = "text" placeholder= "Artist Name" value="${artist.artistId}">
+
                               <button class="add-artist-comment__submit">Submit</button>
 
                       </div>
@@ -52,8 +52,18 @@ export default function Artists(artists) {
             `;
     }).join('')}
     </ul>
-    
+    <div class="subheader add-artist">
+        <input class= "add-artist__artistName" type = "text" placeholder= "Artist Name">
+        <input class= "add-artist__artistImage" type = "text" placeholder= "Artist Image">
+        <input class= "add-artist__artistRating" type = "text" placeholder= "Artist Rating">
+        <input class= "add-artist__tag" type = "text" placeholder= "Add a Tag">
+        <button class="add-artist__submit">Submit</button>
+    </div>
+    <div class="subheader add-tag">
+        <input class= "add-tag__tag" type = "text" placeholder= "Add a New Tag">
+        <button class="add-tag__submit">Submit</button>
+    </div>    
 
-   
+
   `;
 }
